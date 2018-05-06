3d35b691b1705b8805f0778af8bcbe45295f94de
lnarvaez <lgnarvaez1990@gmail.com>
2018-05-06 10:01:40 -0500

    Add exception control in main method.

--- src/main/java/MyFirstAppMain.java
+++ src/main/java/MyFirstAppMain.java
@@ -1,7 +1,13 @@
 public class MyFirstAppMain {
 
     public static void main(String args[]) {
-        final String WELCOME_MESSAGE = "Hello, are you ready for the action?";
-        System.out.println(WELCOME_MESSAGE);
+        try {
+            final String WELCOME_MESSAGE = "Hello, are you ready for the action?";
+            System.out.println(WELCOME_MESSAGE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("El método principal ha finalizado.");
+        }
     }
 }
