e903327b9bcfa088035495e6bbc80484427c973d
lnarvaez <lgnarvaez1990@gmail.com>
2018-05-05 19:00:04 -0500

    My first commit

---
+++ src/main/java/MyFirstAppMain.java
@@ -0,0 +1,7 @@
+public class MyFirstAppMain {
+
+    public static void main(String args[]) {
+        final String WELCOME_MESSAGE = "Hello, are you ready for the action?";
+        System.out.println(WELCOME_MESSAGE);
+    }
+}
